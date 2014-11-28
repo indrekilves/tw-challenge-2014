c73e0a50b22adccb8a6a79a0f1015fe2bd8b104e
Indrek Ilves <minimalist6000@gmail.com>
2014-11-28 23:28:11 +0200

    Baseline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwcApp::Application.config.secret_token = 'b633c98f2e40b4634c5858d527dfff03d003f02c96c5191ebfbaa4c1e6c0639eee8213caa3dae7923da85f7eb8e49374c20900073ec128ef8e81dde620119c7e'

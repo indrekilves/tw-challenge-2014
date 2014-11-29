6ccbf8b67a89f142c516527a1f8a85cbe7b17e5d
Roman Normann <roman@raintree.ee>
2014-11-29 20:01:33 +0200

    Controller

--- app/controllers/receive_controller.rb
+++ app/controllers/receive_controller.rb
@@ -1,4 +1,6 @@
 class ReceiveController < ApplicationController
+  require 'net/http'
+
   def index
     render
   end
@@ -10,7 +12,27 @@ class ReceiveController < ApplicationController
   end
 
   def transfer
+    sourceCurrency = params[:sourceCurrencyId]
+    sourceValue   = params[:sourceValue]
+    userName      = params[:userName]
 
+    json ="{ \"sender\": { \"name\": \"Paul\", \"account\": \"ABC123456789\" },
+             \"receiver\": { \"name\": \""  + userName + "\", \"account\": \"MADREMIA123\"},
+             \"amount\": 120.7,
+             \"currency\": \"EUR\"}"
+    #ActiveSupport::JSON.encode({ team: 'rails', players: '36' })
+
+    jsonEsc = CGI::escape(json)
+    testHost = 'localhost:3000/mineperseurl/'
+    realHost = 'http://challenge.transferwise.com/'
+    fullUrl = realHost + "?teamname=bootstrap&data=" + jsonEsc
+    #fullUrl = Rack::Utils.escape(fullUrl)
+
+
+    result = Net::HTTP.get(URI.parse(fullUrl))
+
+    #my_connection = Net::HTTP.new('localhost\mineperseurl', 3000)
+    #reponse = my_connection.get("teamname=bootstrap&data=" + json)
     render "public/sender2.html"
   end
 end
